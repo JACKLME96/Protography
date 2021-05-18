f508e224f6f5b1e7339a216cf24b1055b1e9e529
Tommaso <t.carboni@campus.unimib.it>
2021-05-18 10:43:36 +0200

    Aggiunta sezione Login
    Aggiunta Login, UserRegistration e ForgotPassword Activity. Aggiunta classe User

---
+++ app/src/main/java/com/example/protography/ForgotPassword.java
@@ -0,0 +1,77 @@
+package com.example.protography;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private Button resetPasswordButton;
+    
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        emailEditText = (EditText) findViewById(R.id.email);
+        resetPasswordButton = (Button) findViewById(R.id.resetButton);
+
+
+        auth = FirebaseAuth.getInstance();
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+
+            private void resetPassword() {
+
+                String email = emailEditText.getText().toString().trim();
+
+                if(email.isEmpty()){
+                    emailEditText.setError("Email richiesta.");
+                    emailEditText.requestFocus();
+                    return;
+                }
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    emailEditText.setError("Email non valida. Riprovate");
+                    emailEditText.requestFocus();
+                    return;
+                }
+
+
+                auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ForgotPassword.this, "Controlla la mail per il reset della password", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                            Toast.makeText(ForgotPassword.this, "Errore, riprova", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/protography/UserRegistration.java
@@ -0,0 +1,125 @@
+package com.example.protography;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
+
+public class UserRegistration extends AppCompatActivity implements View.OnClickListener {
+
+
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextEmail, editTextPassword;
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView)findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+        registerUser = (Button) findViewById(R.id.register);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.NomeCompleto);
+        editTextEmail = (EditText) findViewById(R.id.registerAddress);
+        editTextPassword = (EditText) findViewById(R.id.registerPassword);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.register:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String nomeCompleto = editTextFullName.getText().toString().trim();
+
+        if(nomeCompleto.isEmpty()){
+            editTextFullName.setError("Nome non valido. il campo non deve essere vuoto.");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password non valida. il campo non deve essere vuoto.");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            editTextEmail.setError("Email non valida. il campo non deve essere vuoto.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Mail non valida");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Lunghezza minima della psw è di 6 caratteri ");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(nomeCompleto,email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(UserRegistration.this, "Utente registrato con successo,", Toast.LENGTH_LONG).show();
+
+                                    } else {
+                                        Toast.makeText(UserRegistration.this, "Registrazione fallita", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }else {
+                            Toast.makeText(UserRegistration.this, "Registrazione fallita", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+    }
+
+}
